26655ccf818322635a9186a543a0fd66872a7eac
KoptevAnton <koptev110593@gmail.com>
2025-01-10 18:38:49 +0100

    Complete Node.js HW #1.

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,11 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  const existingContacts = await readContacts();
+  const createdContact = createFakeContact();
+  await writeContacts([...existingContacts, createdContact]);
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,11 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
-generateContacts(5);
+const generateContacts = async (number) => {
+  const existingContacts = await readContacts();
+  const generatedContacts = Array(number).fill(0).map(createFakeContact);
+  await writeContacts([...existingContacts, ...generatedContacts]);
+};
+
+generateContacts(3);
